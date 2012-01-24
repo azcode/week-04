c67ad5b5196245107b8e930e53eec25924f768bd
Uddin <muddin200@localhost.(none)>
2012-01-24 13:01:24 -0500

    fixed superheroes

--- lib/display_superheroes.rb
+++ lib/display_superheroes.rb
@@ -8,3 +8,18 @@
 # @see http://en.wikibooks.org/wiki/Ruby_Programming/Syntax/Method_Calls
 # @see Programing Ruby, Chapter 6 
 #
+
+def display_superheroes(superhero, some_more_superheroes = nil)
+  [superhero, some_more_superheroes].each do |hero|
+  
+  hero_hash = {}
+  
+  if hero.is_a? Array
+    hero.each do |supes|
+      hero_hash[supes.name] = supes
+    end
+  else
+    hero_hash[hero.name] = hero
+  end unless hero.nil?
+  end
+end
